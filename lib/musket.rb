9bdad1fbb718aec255b0df5b7f95048c227f9016
Aniket Pant <me@aniketpant.com>
2013-06-04 21:29:55 +0530

    Add setup function
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- lib/musket.rb
+++ lib/musket.rb
@@ -1,7 +1,18 @@
 require 'mote'
+require 'fileutils'
 
 module Musket
-  def self.do
-    "Start writing the gem"
+  def self.initialize
+    config_dir = Dir.home + '/.musket/'
+    config_file = config_dir + 'config.yml'
+    if File.exist?config_file
+      config = File.open(config_file, 'r')
+    else
+      puts "#{config_file} does not exist. Creating file."
+      FileUtils.mkdir(config_dir)
+      config = File.open(config_file, 'w')
+      config.puts('# This is the musket configuration file')
+      config.close
+    end
   end
 end
